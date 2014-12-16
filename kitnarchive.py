70481456b03f9764561b08fb8c310520719c8b62
Finn Ellis <finnre@cat.pdx.edu>
2014-12-16 05:22:38 -0800

    Reply in channel and remove queue.

--- kitnarchive.py
+++ kitnarchive.py
@@ -24,15 +24,14 @@ class ArchiveModule(Module):
         self.infobot = config.get('redmine', 'infobot')
         self.waiting = None
         self.info = ""
-        self.requester = None
-        self.queue = []
+        self.requester = ()
 
     def append_page(self, title, new_text, project_id=None, comment=None, requester=None):
         if project_id == None:
             project_id = self.project
         if comment == None:
             if requester:
-                comment = "Updated by {} at {}'s request.".format(self.controller.config.get('server', 'nick'), requester)
+                comment = "Updated by {} at {}'s request.".format(self.controller.config.get('server', 'nick'), requester[0])
             else:
                 comment = "Updated by {}.".format(self.controller.config.get('server', 'nick'))
         page = self.host.wiki_page.get(title, project_id=project_id)
@@ -42,19 +41,16 @@ class ArchiveModule(Module):
 
     def archive(self):
         factoids = re_split(r'( or |\|)', self.info)[::2]
-        formatted_info = "\n\n{} reported on {} that {} is:\n* ".format(self.controller.config.get('server', 'nick'), datetime.today().date(), self.waiting) + "\n* ".join(factoids)
+        formatted_info = "\n\n{} recorded on {} that {} is:\n* ".format(self.controller.config.get('server', 'nick'), datetime.today().date(), self.waiting) + "\n* ".join(factoids)
         self.append_page('api_test', formatted_info, requester=self.requester)
-        self.controller.client.msg(self.requester, "Done archiving {} for you!".format(self.waiting))
+        self.controller.client.reply(self.requester[1], self.requester[0], "Done!".format(self.waiting))
         self.clear()
 
     def clear(self):
         _log.debug("Clearing waiting stuff.")
         self.waiting = None
-        self.requester = None
+        self.requester = ()
         self.info = ""
-        for nick in self.queue:
-            self.controller.client.msg(nick, "Sorry about that, all done now. You can request archives again.")
-        self.queue = []
 
     @Module.handle('PRIVMSG')
     def handle_privmsg(self, client, actor, recipient, message):
@@ -70,7 +66,6 @@ class ArchiveModule(Module):
                 client.reply(recipient, actor, "I'm not actually trying to archive anything right now.")
             else:
                 client.reply(recipient, actor, "Okay, I'll stop trying to archive {}.".format(self.waiting))
-                self.controller.client.msg(self.requester, "Sorry, I tried to archive {} for you and gave up. :(".format(self.waiting))
                 self.clear()
 
         elif self.waiting and actor == self.infobot and str(recipient) == client.user.nick:
@@ -88,16 +83,12 @@ class ArchiveModule(Module):
 
         elif message.startswith("archive"):
             if self.waiting != None:
-                client.reply(recipient, actor, "Hang on, I'm still archiving {}. I'll tell you when I'm done.".format(self.waiting))
-                queued = actor.split("!", 1)[0]
-                if queued not in self.queue:
-                    self.queue.append(queued)
+                client.reply(recipient, actor, "Hang on, I'm still archiving {}.".format(self.waiting))
             else:
                 key = message.split(None, 1)[1]
                 self.waiting = key
-                self.requester = actor.split("!", 1)[0]
+                self.requester = (actor.split("!", 1)[0], recipient)
                 infonick = self.infobot.split("!", 1)[0]
-                _log.debug("Got a request from {} to archive {}, asking {}".format(self.requester, key, infonick))
                 self.controller.client.msg(infonick, "{}: literal {}".format(infonick, key))
 
         else:
